807679680c38505ad6e5861510a30648d2522041
jeverill0462954 <w0462954@campus.nscc.ca>
2024-01-28 22:31:38 -0400

    Functionality implemented

--- gamepicker.py
+++ gamepicker.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from urllib.request import urlopen
 import json
 import re
@@ -18,34 +18,54 @@ def cleanhtml(raw_html):
     cleantext = re.sub(CLEANR, '', raw_html)
     return cleantext
 
+
+
 # summarize the description of two games, and compare them to see which one is "the most interesting"
-def pickGame():
+def pickGame(appid_1,appid_2,questionInput):
     print("Reading JSON data...")
     # get json data from game 1 and 2
-    with urlopen("https://store.steampowered.com/api/appdetails?appids=220") as url:
-        game1_data = json.load(url)['220']['data']
+    with urlopen("https://store.steampowered.com/api/appdetails?appids={0}".format(appid_1)) as url:
+        game1_data = json.load(url)[appid_1]['data']
 
-    with urlopen("https://store.steampowered.com/api/appdetails?appids=1091500") as url:
-        game2_data = json.load(url)['1091500']['data']
-
-    # get summaries
-    print("Summarizing descriptions...")
-    game1_summary = summary_model(cleanhtml(game1_data['detailed_description']), max_length=300, min_length=50, do_sample=False)[0]['summary_text']
-    game2_summary = summary_model(cleanhtml(game2_data['detailed_description']), max_length=300, min_length=50, do_sample=False)[0]['summary_text']
+    with urlopen("https://store.steampowered.com/api/appdetails?appids={0}".format(appid_2)) as url:
+        game2_data = json.load(url)[appid_2]['data']
 
     # ask question
     print("Querying...")
-    print(qa_model(
-            question = "What game sounds more interesting, {0} or {1}?".format(game1_data['name'],game2_data['name']),
-            context = "The description for {0} reads: '{1}' The description for {2} reads '{3}'".format(game1_data['name'],game1_summary,game2_data['name'],game2_summary)
-        )['answer']
-    )
+    return qa_model(
+        question = "What game sounds more {0}, {1} or {2}?".format(questionInput,game1_data['name'],game2_data['name']),
+        context = "The description for {0} reads: '{1}' The description for {2} reads '{3}'".format(
+            game1_data['name'],
+            cleanhtml(game1_data['detailed_description']),
+            game2_data['name'],
+            cleanhtml(game2_data['detailed_description'])
+        )
+    )['answer']
 
 # ------ FLASK STUFF BEGINS HERE ------
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        questionInput = request.form['questionInput']
+        
+        # create regex for getting the appid from a store link
+        regex = re.compile(r"/app/(\d+)/")
+        appid_1 = regex.search(request.form['url1'])
+        appid_2 = regex.search(request.form['url2'])
+
+        if appid_1 and appid_2:
+            appid_1 = appid_1.group(1)
+            appid_2 = appid_2.group(1)
+
+            ai_answer = f"I think {pickGame(appid_1,appid_2,questionInput)} is more {questionInput}."
+
+            return render_template('index.html', answer=ai_answer)
+        else:
+            return render_template('index.html', answer="Sorry, I think your inputs are invalid.")
+
 if __name__ == '__main__':
     app.run()
